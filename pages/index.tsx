56905449c80b2e56289cd20f7a32c0066ebf5067
SardiusJay <abidoyejay@gmail.com>
2023-04-28 22:03:28 +0100

    inital commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main>
-				Hello dlcf
+				Hello dlcf, You are welcome devs
       </main>
     </>
   )
